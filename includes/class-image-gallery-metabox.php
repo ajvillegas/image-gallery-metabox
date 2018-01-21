dce1ad06206d76fc07d551577ee46e8ec8ecbafb
Alexis J. Villegas <alexis@ajvillegas.com>
2018-01-20 17:30:08 -0800

    Update version number

--- includes/class-image-gallery-metabox.php
+++ includes/class-image-gallery-metabox.php
@@ -69,7 +69,7 @@ class Image_Gallery_Metabox {
 	public function __construct() {
 
 		$this->plugin_name = 'image-gallery-metabox';
-		$this->version = '1.0.0';
+		$this->version = '1.0.1';
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
