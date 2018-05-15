d1455d7b57bc49fad68d5d50bb7f81ecb00f44c7
Alexis J. Villegas <alexis@ajvillegas.com>
2018-05-15 11:36:37 -0700

    Add GitHub Updater URI to plugin header

--- image-gallery-metabox.php
+++ image-gallery-metabox.php
@@ -23,6 +23,7 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       image-gallery-metabox
  * Domain Path:       /languages
+ * GitHub Plugin URI: ajvillegas/image-gallery-metabox
  */
 
 // If this file is called directly, abort.
